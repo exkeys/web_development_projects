9ded83f6fa9dfe7935db846021ef0acc1cbd1be3
EXKEYS <rjsdn6422@naver.com>
2025-04-04 21:35:51 +0900

    feat: 20250404 delte function finished

---
+++ combined_project/frontend/cardata/src/api/carapi.ts
@@ -0,0 +1,14 @@
+import { CarResponse } from "../types";
+import axios from "axios";
+
+export const getCars = async (): Promise<CarResponse[]> => {
+    const response = await axios.get(`${import.meta.env.VITE_API_URL}/api/cars`);  // localhost 대체 부분
+
+    return response.data._embedded.cars;  
+  }
+
+
+export const deleteCar = async (link: string): Promise<CarResponse> => {
+  const response = await axios.delete(link);
+  return response.data
+}
